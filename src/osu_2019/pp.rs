dbce0375fbd2863b5f31c4185dfacb8fa4e99a5b
tsunyoku <mbruhyo@gmail.com>
2022-11-20 15:23:42 +0000

    feat: increase non-dt bonus

--- src/osu_2019/pp.rs
+++ src/osu_2019/pp.rs
@@ -276,7 +276,7 @@ impl<'m> OsuPP<'m> {
         }
 
         let nodt_bonus = match !self.mods.change_speed() && self.mods.rx() {
-            true => 1.01,
+            true => 1.02,
             false => 1.0,
         };
 
