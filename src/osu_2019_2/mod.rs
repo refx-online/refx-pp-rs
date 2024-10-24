6d414d17c66dd32c8c96e4738091eeaad1b3fb3b
ldci4 <anoletenu@gmail.com>
2024-10-24 18:10:23 +0700

    uses 2019

---
+++ src/osu_2019_2/mod.rs
@@ -0,0 +1,19 @@
+mod curve;
+use curve::Curve;
+
+mod difficulty_object;
+use difficulty_object::DifficultyObject;
+
+mod osu_object;
+use osu_object::OsuObject;
+
+mod pp;
+pub use pp::{OsuAttributeProvider, OsuPP};
+
+mod skill;
+use skill::Skill;
+
+mod skill_kind;
+use skill_kind::SkillKind;
+
+pub mod stars;
