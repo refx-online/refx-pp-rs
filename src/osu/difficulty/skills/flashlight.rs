129b843f24edceadfdc387b62e7221608243d025
Uranium <190546707+SkibidiWatesiggma@users.noreply.github.com>
2026-05-10 15:45:48 +0200

    fix missing ;

--- src/osu/difficulty/skills/flashlight.rs
+++ src/osu/difficulty/skills/flashlight.rs
@@ -155,7 +155,7 @@ impl FlashlightEvaluator {
 
                 result += opacity_bonus * self.scaling_factor * jump_dist
                     / (cumulative_strain_time + 50.0).sqrt();
-                result *= stack_nerf
+                result *= stack_nerf;
 
                 if let Some((curr_obj_angle, osu_curr_angle)) = curr_obj.angle.zip(osu_curr.angle) {
                     // * Objects further back in time should count less for the nerf.
